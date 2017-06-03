8704a962757fb959c1373188ca429164dd5be63f
janovas <janovas@outlook.com>
2017-06-03 23:17:02 +0700

    Update color_detection.ino

--- color_detection.ino
+++ color_detection.ino
@@ -39,9 +39,9 @@ void loop() {
   digitalWrite(S3, LOW);
   delay(10);
   // Reading the output frequency
-  R_componentRAW[index] = pulseIn(sensorOut, LOW);
+  R_componentRAW[index] =(float)pulseIn(sensorOut, LOW);
   //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequency = map(frequency, 25,72,255,0);
+  R_componentRAW[index] = map(R_componentRAW[index], 29,310,255,0);
   // Printing the value on the serial monitor
   //  Serial.print("R="); // Printing Name
   //  Serial.print(R_component); //Printing RED color frequency
@@ -53,9 +53,9 @@ void loop() {
   digitalWrite(S3, HIGH);
   delay(10);
   // Reading the output frequency
-  G_componentRAW[index] = pulseIn(sensorOut, LOW);
+  G_componentRAW[index] =(float)pulseIn(sensorOut, LOW);
   //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequency = map(frequency, 30,90,255,0);
+  G_componentRAW[index]  = map(G_componentRAW[index] , 30,323,255,0);
   // Printing the value on the serial monitor
   //  Serial.print("G="); // Printing Name
   //  Serial.print(G_component); //Printing RED color frequency
@@ -67,9 +67,9 @@ void loop() {
   digitalWrite(S3, HIGH);
   delay(10);
   // Reading the output frequency
-  B_componentRAW[index] =pulseIn(sensorOut, LOW);
+  B_componentRAW[index] = (float)pulseIn(sensorOut, LOW);
   //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequency = map(frequency, 25,70,255,0);
+  B_componentRAW[index] = map(B_componentRAW[index], 21,226,255,0);
   // Printing the value on the serial monitor
   //  Serial.print("B="); // Printing Name
   //  Serial.print(B_component); //Printing RED color frequency
